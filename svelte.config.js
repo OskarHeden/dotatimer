538e8b98c88fd68607827bb6063b17c33c2fd193
Oskar Hedén <oskar.heden@hyperlab.se>
2023-04-03 14:52:52 +0200

    Revert to old svelte.config

--- svelte.config.js
+++ svelte.config.js
@@ -1,31 +1,15 @@
-import adapter from '@sveltejs/adapter-static';
-import { VitePWA } from 'vite-plugin-pwa';
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
-export default {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
 	kit: {
-		adapter: adapter(),
-		target: '#svelte',
-		vite: {
-			plugins: [
-				VitePWA({
-					manifest: {
-						// Add your app details here
-						name: 'DotaTimer PWA',
-						short_name: 'DotaTimerPWA',
-						description: 'A Progressive Web App built with SvelteKit',
-						theme_color: '#ffffff',
-						background_color: '#ffffff',
-						display: 'standalone',
-						start_url: '/',
-						icons: [
-							// Add your icons here
-						]
-					},
-					workbox: {
-						// Workbox options
-					}
-				})
-			]
-		}
+		adapter: adapter()
 	}
 };
+
+export default config;
