c2aae2bdb225ea808f2ec2854891c609a37fdaec
Oskar Hedén <oskar.heden@hyperlab.se>
2023-04-03 14:04:58 +0200

    Bait commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>OK Christoffer, you're up!</h1>
+<span>Show me some TIMERS!</span>
