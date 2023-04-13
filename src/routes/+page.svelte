3c686cb2d72cfb6ee01f2a0b4dc8a3395efd8f1c
Oskar Hedén <oskar.heden@hyperlab.se>
2023-04-13 13:33:04 +0200

    Service worker - check for updates to app

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,31 @@
 	import './global.css';
 	import StackingTimer from '../components/StackingTimer.svelte';
 	import GameTimer from '../components/Gametimer.svelte';
+
+	$: if (typeof navigator !== 'undefined' && 'serviceWorker' in navigator) {
+		console.log('mupp');
+		navigator.serviceWorker
+			.register('/service-worker.ts')
+			.then((registration) => {
+				console.log('Service Worker registered:', registration);
+
+				// Listen for updates from the service worker
+				registration.active?.addEventListener('message', (event) => {
+					if (event.data.action === 'updateAvailable') {
+						console.log('Update available! Refresh the page to get the latest version.');
+						// Notify the user or refresh the page to update the app
+					}
+				});
+
+				// Check for updates every minute (60,000 milliseconds)
+				setInterval(() => {
+					registration.active?.postMessage({ action: 'checkForUpdate' });
+				}, 60000);
+			})
+			.catch((error) => {
+				console.log('Service Worker registration failed:', error);
+			});
+	}
 </script>
 
 <div class="header">
--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,3 +1,81 @@
-self.addEventListener('fetch', function () {
-	return;
+// sw.js - Service Worker File
+
+// Cache version - change this value when you want to force an update
+const CACHE_VERSION = 'v1';
+
+// Cache names
+const CACHE_STATIC_NAME = `static-${CACHE_VERSION}`;
+const CACHE_DYNAMIC_NAME = `dynamic-${CACHE_VERSION}`;
+
+// Assets to cache
+const STATIC_ASSETS = ['/'];
+
+// Install event
+self.addEventListener('install', (event) => {
+	event.waitUntil(
+		caches.open(CACHE_STATIC_NAME).then((cache) => {
+			console.log('Caching static assets');
+			return cache.addAll(STATIC_ASSETS);
+		})
+	);
+});
+
+// Activate event
+self.addEventListener('activate', (event) => {
+	event.waitUntil(
+		caches.keys().then((cacheNames) => {
+			return Promise.all(
+				cacheNames.map((cacheName) => {
+					if (cacheName !== CACHE_STATIC_NAME && cacheName !== CACHE_DYNAMIC_NAME) {
+						console.log('Removing old cache:', cacheName);
+						return caches.delete(cacheName);
+					}
+				})
+			);
+		})
+	);
+});
+
+// Fetch event
+self.addEventListener('fetch', (event) => {
+	event.respondWith(
+		caches.match(event.request).then((response) => {
+			if (response) {
+				return response;
+			} else {
+				return fetch(event.request)
+					.then((res) => {
+						return caches.open(CACHE_DYNAMIC_NAME).then((cache) => {
+							cache.put(event.request.url, res.clone());
+							return res;
+						});
+					})
+					.catch(() => {
+						console.log('Fetch failed');
+						return null;
+					});
+			}
+		})
+	);
+});
+
+// Check for updates
+self.addEventListener('message', (event) => {
+	if (event.data.action === 'checkForUpdate') {
+		console.log('Checking for updates...');
+		fetch(self.registration.scope).then((response) => {
+			if (response.status === 200) {
+				response.text().then((text) => {
+					if (text.indexOf(CACHE_VERSION) === -1) {
+						console.log('Update available, notifying the app...');
+						self.clients.matchAll().then((clients) => {
+							clients.forEach((client) => client.postMessage({ action: 'updateAvailable' }));
+						});
+					} else {
+						console.log('No update available');
+					}
+				});
+			}
+		});
+	}
 });
