42c38152debbc7caf5d807fe9fefb9c8b1b67504
Oskar Hedén <oskar.heden@hyperlab.se>
2023-04-17 08:27:39 +0200

    Try not pausing

--- src/helpers/sound.ts
+++ src/helpers/sound.ts
@@ -20,14 +20,17 @@ export function unlockSounds() {
 	function unlockAll() {
 		if (audiosWeWantToUnlock) {
 			for (let audio of audiosWeWantToUnlock) {
+				console.log('Playing', audio);
 				audio.play();
-				audio.pause();
-				audio.currentTime = 0;
+				// audio.pause();
+				// audio.currentTime = 0;
 			}
 			audiosWeWantToUnlock = null;
+			console.log('removing eventlistener for audio unlock');
 			document.body.removeEventListener('touchstart', unlockAll);
 		}
 	}
+	console.log('Adding eventlistener for audio unlock');
 	document.body.addEventListener('touchstart', unlockAll, false);
 
 	const aegisRecalimed = new Audio('../../static/sound/Joey/aegisReclaimed.mp3');
