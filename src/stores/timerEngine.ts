14fb858f7e63566d99bc881e1bc5dbbff73f8db8
Alexander Woodhouse <alexander.woodhouse@hyperlab.se>
2023-04-17 20:59:51 +0200

    Prioritize enabled timers in sorting

--- src/stores/timerEngine.ts
+++ src/stores/timerEngine.ts
@@ -18,43 +18,52 @@ const formatTime = (value: number) => value.toString().padStart(2, '0');
 export const timerEngine: Readable<Timer[]> = derived(
 	[timerConfig, gameTimer],
 	([$timerConfigs, $gameTimer]): Timer[] => {
-		return $timerConfigs.map((timer, index) => {
-			let remainingSeconds: number;
+		return $timerConfigs
+			.map((timer, index) => {
+				let remainingSeconds: number;
 
-			// Calculate the total elapsed rounds for the current game timer value
-			const elapsedRounds = Math.floor($gameTimer.time / (timer.interval * 60));
+				// Calculate the total elapsed rounds for the current game timer value
+				const elapsedRounds = Math.floor($gameTimer.time / (timer.interval * 60));
 
-			// Check if the timer should still skip rounds
-			if (elapsedRounds < timer.initialSkip) {
-				remainingSeconds =
-					timer.interval * 60 * (timer.initialSkip - elapsedRounds) +
-					timer.interval * 60 -
-					$gameTimer.time;
-			} else {
-				// Calculate the remaining seconds for the current round
-				const timeInCurrentRound = $gameTimer.time % (timer.interval * 60);
-				remainingSeconds = timer.interval * 60 - timeInCurrentRound;
-			}
+				// Check if the timer should still skip rounds
+				if (elapsedRounds < timer.initialSkip) {
+					remainingSeconds =
+						timer.interval * 60 * (timer.initialSkip - elapsedRounds) +
+						timer.interval * 60 -
+						$gameTimer.time;
+				} else {
+					// Calculate the remaining seconds for the current round
+					const timeInCurrentRound = $gameTimer.time % (timer.interval * 60);
+					remainingSeconds = timer.interval * 60 - timeInCurrentRound;
+				}
 
-			const minutesLeft = Math.floor(remainingSeconds / 60);
-			const secondsLeft = remainingSeconds % 60;
-			const remainingFormatted = `${formatTime(minutesLeft)}:${formatTime(secondsLeft)}`;
+				const minutesLeft = Math.floor(remainingSeconds / 60);
+				const secondsLeft = remainingSeconds % 60;
+				const remainingFormatted = `${formatTime(minutesLeft)}:${formatTime(secondsLeft)}`;
 
-			let flash = false;
-			
-			if(timer.enabled) {
-				flash = remainingSeconds < FLASH_WHEN_LESS_THAN;
-				if (remainingSeconds === 1) playSoundEffect(new Audio(`/sound/Joey/${timer.audio}`));
-			}
+				let flash = false;
 
-			return {
-				...timer,
-				index,
-				remainingSeconds,
-				remainingFormatted,
-				flash
-			};
-		})
-		.sort((a, b) => a.remainingSeconds > b.remainingSeconds ? 1 : -1);
+				if (timer.enabled) {
+					flash = remainingSeconds < FLASH_WHEN_LESS_THAN;
+					if (remainingSeconds === 1) playSoundEffect(new Audio(`/sound/Joey/${timer.audio}`));
+				}
+
+				return {
+					...timer,
+					index,
+					remainingSeconds,
+					remainingFormatted,
+					flash
+				};
+			})
+			.sort((a, b) =>
+				a.enabled !== b.enabled
+					? b.enabled
+						? 1
+						: -1
+					: a.remainingSeconds > b.remainingSeconds
+						? 1
+						: -1
+			);
 	}
 );
