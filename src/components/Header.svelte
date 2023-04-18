e1f563f566f368f3ea57ede3f8aaa7564f3505a6
Christoffer Ljung <christoffer.ljung@hyperlab.se>
2023-04-18 11:44:54 +0200

    update header image

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,24 +1,24 @@
 <script>
-		import GameTimer from '../components/GameTimer.svelte';
+	import GameTimer from '../components/GameTimer.svelte';
 </script>
 
 <div class="header">
-    <img src="clockUI.png" alt="timer" />
+	<img src="Dota-UI.png" alt="timer" />
 	<GameTimer />
 </div>
 
 <style>
-    .header {
-        position: relative;
-        width: 100%;
-        max-width: 460px;
-        aspect-ratio: 467 / 89;
-        margin: auto;
-    }
-    img {
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-    }
+	.header {
+		position: relative;
+		width: 100%;
+		max-width: 460px;
+		aspect-ratio: 484 / 96;
+		margin: auto;
+	}
+	img {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+	}
 </style>
