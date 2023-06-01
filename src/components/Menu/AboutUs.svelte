14fc9529f71b49030b8a4a67fd2673f7e05158f3
Christoffer Ljung <christoffer.ljung@hyperlab.se>
2023-06-01 14:02:39 +0200

    about us content and BMAC

--- src/components/Menu/AboutUs.svelte
+++ src/components/Menu/AboutUs.svelte
@@ -1,7 +1,72 @@
+<script
+	type="text/javascript"
+	src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js"
+	data-name="bmc-button"
+	data-slug="dotatimer"
+	data-color="#FFDD00"
+	data-emoji
+	data-font="Cookie"
+	data-text="Buy me a coffee"
+	data-outline-color="#000000"
+	data-font-color="#000000"
+	data-coffee-color="#ffffff"
+></script>
+
 <h2>About us</h2>
+<div class="aboutUs">
+	<p>Greetings, fellow feeders!</p>
+	<p>
+		Welcome to the magical realm of Dotatimer, your trusty sidekick for tracking Dota 2 events. We
+		are a party of two, armed with the double-edged sword of gaming passion and tech wizardry. Like
+		two halves of a Heart of Tarrasque, we've fused our love for Dota 2 and our knack for creating
+		awesome tech to forge Dotatimer.
+	</p>
+	<p>
+		Have you ever found yourself yearning for a sixth sense to predict Roshan's respawn? Or wished
+		for a crystal ball to foresee the appearance of Power Runes, Wisdom Runes, or Bounty Runes?
+		Well, your prayers to GabeN have been answered!
+	</p>
+	<p>
+		Dotatimer is here, a creation forged in the fiery crucible of caffeine-fueled coding marathons
+		and relentless debugging battles. It's more than just a timer - it's your strategic companion in
+		this grand theater of endless battle. Our mission is simple: to let you focus on epic jukes and
+		Godlike sprees, while we handle the mundane reality of game event tracking.
+	</p>
+	<p>
+		We believe in the power of community (after all, it's not a solo game, is it?), and we are
+		committed to refining and upgrading Dotatimer based on your sage advice. Your feedback is as
+		valuable to us as a well-placed Observer Ward.
+	</p>
+	<p>
+		If Dotatimer assists you in clutch plays and you feel like showering some love, consider "Buying
+		us a coffee". Every coin counts in our quest to maintain and improve this game-changing tool,
+		and trust us, it's a lot cheaper than buying a Divine Rapier!
+	</p>
+	<p>
+		Give Dotatimer a spin today, and take your Dota 2 experience from ‘feeding’ to 'EZ Game, EZ
+		Life'.
+	</p>
+	<a href="https://www.buymeacoffee.com/dotatimer" target="_blank"
+		><img
+			src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
+			alt="Buy Me A Coffee"
+			style="height: 60px !important;width: 217px !important;"
+		/></a
+	>
+</div>
 
 <style>
 	h2 {
 		color: white;
+		margin-left: 5%;
+	}
+	p {
+		color: white;
+	}
+	.aboutUs {
+		max-height: 75svh;
+		overflow: scroll;
+		padding-left: 5%;
+		padding-right: 5%;
 	}
 </style>
