3dfc507b8d03c216a4e5c24b05d0612bd39a3d6a
Oskar Hedén <oskar.heden@hyperlab.se>
2023-06-01 23:27:03 +0200

    About us update?

--- src/components/Menu/AboutUs.svelte
+++ src/components/Menu/AboutUs.svelte
@@ -13,12 +13,9 @@
 ></script>
 
 <div class="aboutUs">
-	<p>Greetings, fellow feeders!</p>
+	<h2>DotaTimer</h2>
 	<p>
-		Welcome to the magical realm of Dotatimer, your trusty sidekick for tracking Dota 2 events. We
-		are a party of two, armed with the double-edged sword of gaming passion and tech wizardry. Like
-		two halves of a Heart of Tarrasque, we've fused our love for Dota 2 and our knack for creating
-		awesome tech to forge Dotatimer.
+		Welcome to the magical realm of Dotatimer, your trusty sidekick for tracking Dota 2 objectives.
 	</p>
 	<p>
 		Have you ever found yourself yearning for a sixth sense to predict Roshan's respawn? Or wished
@@ -29,12 +26,18 @@
 		Dotatimer is here, a creation forged in the fiery crucible of caffeine-fueled coding marathons
 		and relentless debugging battles. It's more than just a timer - it's your strategic companion in
 		this grand theater of endless battle. Our mission is simple: to let you focus on epic jukes and
-		Godlike sprees, while we handle the mundane reality of game event tracking.
+		Godlike sprees, while we handle the mundane reality of keeping a close eye on every objective.
 	</p>
+	<h2>The Curators</h2>
 	<p>
-		We believe in the power of community (after all, it's not a solo game, is it?), and we are
-		committed to refining and upgrading Dotatimer based on your sage advice. Your feedback is as
-		valuable to us as a well-placed Observer Ward.
+		We are a party of two, armed with the double-edged sword of gaming passion and tech wizardry.
+		Like two halves of a Heart of Tarrasque, we've fused our love for Dota 2 and our knack for
+		creating awesome tech to forge DotaTimer.
+	</p>
+
+	<p>
+		We believe in the power of community, and we are committed to refining and upgrading Dotatimer
+		based on your sage advice. Your feedback is as valuable to us as a well-placed Observer Ward.
 	</p>
 	<p>
 		If Dotatimer assists you in clutch plays and you feel like showering some love, consider "Buying
@@ -52,14 +55,27 @@
 			style="height: 60px !important;width: 217px !important;"
 		/></a
 	>
+	<div class="mobileFiller" />
 </div>
 
 <style>
-	p {
+	p,
+	h2 {
 		color: white;
 	}
 	.aboutUs {
-		max-height: 75svh;
+		max-height: 90vh;
 		overflow: scroll;
+	}
+
+	.mobileFiller {
+		display: none;
+	}
+	@media (max-width: 600px) {
+		.mobileFiller {
+			display: block;
+			height: 4rem;
+			width: 100%;
+		}
 	}
 </style>
